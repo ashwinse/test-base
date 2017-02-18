b24ee398606759448bb4c7716f78f85dd04311fe
Ashwin Sebastian <asebastian@sysgain.com>
2017-02-18 13:14:40 +0530

    up

--- infromatica-td/scripts/info-container-crt.ps1
+++ infromatica-td/scripts/info-container-crt.ps1
@@ -1,22 +1,59 @@
-workflow container1 {
+workflow container3 {
     param(
      
        
 
         [Parameter(Mandatory=$true)]
         [string]
-        $adfStorageAccName,
+        $src1Container,
 
         [Parameter(Mandatory=$true)]
         [string]
-        $adfStorageAccKey
+        $src2Container,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $destContainer,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $source1StorageAccountName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $source1StorageAccountKey,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $source2StorageAccountName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $source2StorageAccountKey,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $destStorageAccountName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $destStorageAccountKey
+
+        
     )
 
     InlineScript{
 
    
-    $adfStorageAccName = $Using:adfStorageAccName
-	$adfStorageAccKey = $Using:adfStorageAccKey
+    $src1Container = $Using:src1Container
+    $src2Container = $Using:src2Container
+    $destContainer = $Using:destContainer
+	$source1StorageAccountName = $Using:source1StorageAccountName
+    $source1StorageAccountKey = $Using:source1StorageAccountKey
+    $source2StorageAccountName = $Using:source2StorageAccountName
+    $source2StorageAccountKey = $Using:source2StorageAccountKey
+    $destStorageAccountName = $Using:destStorageAccountName
+    $destStorageAccountKey = $Using:destStorageAccountKey
 
     Write-Output $adfStorageAccName,
     Write-Output $adfStorageAccKey
