d298c3b715cc1791f3c7903f49a76b32fa31aadf
ashwinse <asebastian@sysgain.com>
2017-10-09 22:30:09 +0530

    up

---
+++ docker-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ubuntu_passwd=$1
+docker_ee_url=$2
+
+repo_url=`echo $docker_ee_url | rev | cut -c5- | rev`
+
+##### Enable 'ubuntu' user to ssh with a password
+echo $ubuntu_passwd | sudo passwd --stdin ubuntu
+file="/etc/ssh/sshd_config"
+passwd_auth="yes"
+cat $file \
+| sed -e "s:\(PasswordAuthentication\).*:PasswordAuthentication $passwd_auth:" \
+> $file.new
+mv $file.new $file
+service sshd restart
+
+
+sudo wget -O /home/ubuntu/copy_certs.sh https://raw.githubusercontent.com/mikegcoleman/hybrid-workshop/master/provision_vms/utilities/copy_certs.sh
+sudo chmod +x copy_certs.sh
+sudo apt-get -y update 
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL $docker_ee_url | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] $repo_url \
+$(lsb_release -cs) \
+stable-17.06"
+sudo apt-get -y update
+sudo apt-get -y install docker-ee
+sudo usermod -aG docker ubuntu
+sudo reboot
