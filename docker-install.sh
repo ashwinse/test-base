262e7388bef20dcba208b2fe3ceef0fd06888602
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-11 13:13:23 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -10,14 +10,14 @@ sudo usermod -l $username ubuntu
 usermod -d /home/$username -m $username
 
 ##### Enable 'docker' user to ssh with a password
-#echo -e "$pwd\n$pwd" | sudo passwd $username
-#file="/etc/ssh/sshd_config"
-#passwd_auth="yes"
-#cat $file \
-#| sed -e "s:\(PasswordAuthentication\).*:PasswordAuthentication $passwd_auth:" \
-#> $file.new
-#mv $file.new $file
-#service sshd restart
+echo -e "$pwd\n$pwd" | sudo passwd $username
+file="/etc/ssh/sshd_config"
+passwd_auth="yes"
+cat $file \
+| sed -e "s:\(PasswordAuthentication\).*:PasswordAuthentication $passwd_auth:" \
+> $file.new
+mv $file.new $file
+service sshd restart
 
 sudo apt-get -y update
 sudo apt-get -y install dnsmasq
