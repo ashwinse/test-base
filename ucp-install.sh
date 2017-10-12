52dee2460d4bae2420c2db134929329149b0461a
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-12 13:58:12 +0530

    Update ucp-install.sh

--- ucp-install.sh
+++ ucp-install.sh
@@ -1,2 +1,5 @@
-"sudo docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.3 install --admin-username docker --admin-password Docker2017 --host-address ${data.oci_core_vnic.lin-nic-c.private_ip_address} --san ${data.oci_core_vnic.lin-nic-c.public_ip_address}",
-"sudo reboot"
+private_ip=$1
+public_ip=$2
+
+sudo docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.3 install --admin-username docker --admin-password Docker2017 --host-address $private_ip --san $public_ip
+sudo reboot
