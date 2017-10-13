69b36ba953de69b3629e34fc5eb202f90bc40bda
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-13 14:30:23 +0530

    Update docker-install-ucp.sh

--- docker-install-ucp.sh
+++ docker-install-ucp.sh
@@ -1,6 +1,9 @@
-docker_ee_url=$1
-private_ip=$2
-public_ip=$3
+#!/bin/sh
+
+username=$1
+docker_ee_url=$2
+manager_private_ip=$3
+manager_public_ip=$4
 
 repo_url=`echo $docker_ee_url | rev | cut -c5- | rev`
 
@@ -13,7 +16,7 @@ echo "server=8.8.4.4" | sudo tee -a /etc/dnsmasq.conf
 sudo apt-get -y update
 sudo service dnsmasq restart
 sudo service networking restart
-
+echo "*********************DNS masq Installed and configured successfully*********************"
 #### open ports for docker
 sudo apt-get purge -y ufw
 sudo apt-get install -y firewalld
@@ -38,7 +41,7 @@ sudo firewall-cmd --add-port=12385/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=12386/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=12387/tcp --permanent --zone=public
 sudo firewall-cmd --reload
-
+echo "*********************Firewall Installed and configured successfully*********************"
 ##### Install docker ee
 sudo wget -O /home/$username/copy_certs.sh https://raw.githubusercontent.com/mikegcoleman/hybrid-workshop/master/provision_vms/utilities/copy_certs.sh
 sudo chmod +x copy_certs.sh
@@ -53,7 +56,11 @@ sudo apt-get -y update
 sudo apt-get -y install docker-ee
 sudo apt-get -y update
 sudo usermod -aG docker $username
+echo "*********************Docker ee Installed and configured successfully*********************"
 sudo docker image pull docker/ucp:2.2.3
-#sudo docker image pull docker/dtr:2.3.3
-sudo docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.3 install --admin-username docker --admin-password Docker2017 --host-address $private_ip --san $public_ip
+
+sudo docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.3 install --admin-username docker --admin-password Docker2017 --host-address $manager_private_ip --san $manager_public_ip
+
+sudo docker swarm join-token worker | sed '1d' | sed '/^\s*$' > docker-join.sh
+
 sudo reboot
