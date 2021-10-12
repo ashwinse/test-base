aa45758db01d9ae9bfaa20e83871b492264a8160
ashwinse <ashwin2se@gmail.com>
2021-10-13 02:20:35 +0530

    up

--- docker_on_CentOs.sh
+++ docker_on_CentOs.sh
@@ -1,4 +1,4 @@
-yum install yum-utils
+yum install -y yum-utils
 yum-config-manager -–add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce
+yum install -y docker
 systemctl start docker
