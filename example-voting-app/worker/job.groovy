ab2108b782c0bdb5bbbeecb1e9b32d0859f21187
ashwinse <ashwin2se@gmail.com>
2022-02-22 19:58:46 -0500

    up

--- example-voting-app/worker/job.groovy
+++
@@ -1,9 +0,0 @@
-node{
-    
-    docker.withRegistry('https://dtrnodednsitqp7hrth6jf2.westus.cloudapp.azure.com/', 'ddc-dtr-login') {
-        git 'https://github.com/SattaRavi/example-voting-app.git'
-    
-        docker.build('admin/com.sysgain.voting-app.worker','./worker').push('latest')
-    }
-    
-}
